8d876fafccba7c82539c0522e2ca659813478475
Oksana Serdiuk <okserdiuk@gmail.com>
2015-10-17 12:41:05 +0300

    first commit

---
+++ localhost/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<form action="action.php" method="get">
+    <p>Hello, what is your name?
+        <input type="text" name="name" /> </p>
+        <input type="submit" />
+</form>
+</body>
+</html>
